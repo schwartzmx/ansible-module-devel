1e87a079fb9353dc381119b0c16a9486319de656
Phil Schwartz <schwartzmx@gmail.com>
2014-12-30 22:19:09 -0600

    working on win_zip module

---
+++ windows/win_zip.ps1
@@ -0,0 +1,106 @@
+#!powershell
+# This file is part of Ansible
+#
+# Copyright 2014, Phil Schwartz <schwartzmx@gmail.com>
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
+# WANT_JSON
+# POWERSHELL_COMMON
+
+$params = Parse-Args $args;
+
+$result = New-Object psobject @{
+    win_zip = New-Object psobject
+    changed = $false
+}
+
+# Requires PSCX, will be installed if it isn't found
+# Can be useful and a must-have for running other useful scripts
+# Pscx-3.2.0.msi
+$url = "http://download-codeplex.sec.s-msft.com/Download/Release?ProjectName=pscx&DownloadId=923562&FileTime=130585918034470000&Build=20959"
+$dest = "C:\Pscx-3.2.0.msi"
+
+# Global flags
+$isLeaf = $false
+$isContainer = $false
+
+# Check if PSCX is installed
+$list = Get-Module -ListAvailable
+# If not download it and install
+If (-Not ($list -match "PSCX")){
+    Try{
+        $client = New-Object System.Net.WebClient
+        $client.DownloadFile($url, $dest)
+    }
+    Catch {
+        Fail-Json $result "Error downloading PSCX from $url and saving as $dest"
+    }
+    Try{
+        msiexec.exe /i $dest /qb
+    }
+    Catch {
+        Fail-Json $result "Error installing $dest"
+    }
+}
+
+# Import
+Try{
+    Import-Module PSCX
+}
+Catch {
+    Fail-Json $result "Error importing module PSCX"
+}
+
+# Get Params
+# SRC
+# Detect if file or directory
+If ($params.src){
+    $src = $params.src.toString()
+
+    If(Test-Path $src -PathType Leaf){
+        $isLeaf = $true
+    }
+    ElseIf (Test-Path $src -PathType Container){
+        $isContainer = $true
+    }
+    Else {
+        Fail-Json $result "Specified src: $src is not a valid file or directory"
+    }
+
+}
+Else {
+    Fail-Json $result "missing required argument: src"
+}
+
+# DEST
+If ($params.dest){
+    $src = $params.dest.toString()
+}
+Else {
+    Fail-Json $result "missing required argument: dest"
+}
+
+# Zip
+Try{
+    # See if write-zip is able to just zip one file
+    Write-Zip -Path $src -OutputPath $dest -IncludeEmptyDirectories
+    # IF write-zip won't work, take look at http://www.codeproject.com/Articles/641275/Create-zip-files-using-PowerShell
+    $result.changed = $true
+}
+Catch {
+    Fail-Json $result "Error zipping $src to $dest"
+}
+
+

6f1b1a31271618b758a9374a096f2703c612a762
schwartzmx <schwartzmx@gmail.com>
2015-01-09 15:20:41 -0600

    more work on win_join

--- windows/win_join.ps1
+++ windows/win_join.ps1
@@ -26,18 +26,21 @@
 # host (optional), can get current hostname and use that
 # domain (optional), people could want to just rename host
 # server (optional), specify name of a domain controller, default none
-# user (optional), could pull from local credentials
-# pass (optional), could pull from local credentials
+# user (optional) required if adding to domain
+# pass (optional) required if adding to domain
 # timezone (optional), could keep as is.  Determine how we should accept input,  Central Standard Time or CST (also case sensitive?)
 # options (optional), Join options Ex. -Options "One, Or, More, Values"
 # oupath (optional), Specifies an organizational unit for the domain account
 # restart (optional), default not
-# unsecure (optional), unsecure join
+# unsecure (optional), unsecure join (use -UnjoinDomainCredential instead of Local or Credential)
 # workgroup (optional), default WORKGROUP
 # -FORCE may be required to supresss user confirmation prompt
 # MAYBE USE -WhatIf to test if command would pass or fail, then run.
 
 $restart = $false
+$domain = $false
+$workgroup = $false
+
 $params = Parse-Args $args;
 
 $result = New-Object psobject @{
@@ -46,13 +49,64 @@ $result = New-Object psobject @{
 }
 
 If ($params.timezone) {
-    C:\Windows\System32\tzutil /s $params.timezone.toString()
+    Try {
+        C:\Windows\System32\tzutil /s $params.timezone.toString()
+    }
+    Catch {
+        Fail-Json $result "Error setting timezone to: $params.timezone : Example: Central Standard Time"
+    }
 }
 
 If ($params.host) {
-    Set-Attr $result.win_join "host" $host.toString()
     $hostname = $params.host.toString()
+    Set-Attr $result.win_join "host" $host.toString()
 }
+
+If ($params.domain -and (-Not($params.workgroup))) {
+    $domain = $params.domain.toString()
+    Set-Attr $result.win_join "domain" $domain.toString()
+}
+ElseIf ($params.workgroup -and (-Not($params.domain))) {
+    $workgroup = $params.workgroup.toString()
+}
+Else {
+    Fail-Json $result "Error with domain/workgroup params. Either both were found or neither were specified."
+}
+
+If ($params.server) {
+    $server = $params.server.toString()
+    Set-Attr $result.win_join "server" $server.toString()
+}
+
+If ($params.user -and $params.pass) {
+    Try {
+        $pass = $params.pass.toString() | ConvertTo-SecureString -asPlainText -Force
+        $creds = New-Object System.Management.Automation.PSCredential($user, $pass)
+    }
+    Catch {
+        Fail-Json $result "error creating PSCredential object from provided credentials, User: $user Password: $params.pass"
+    }
+}
+
+If ($params.options) {
+    $options = $params.options.toString()
+    Set-Attr $result.win_join "options" $options.toString()
+}
+
+If ($params.oupath) {
+    $oupath = $params.oupath.toString()
+    Set-Attr $result.win_join "oupath" $oupath.toString()
+}
+
+If (($params.unsecure -eq "true") -or ($params.unsecure -eq "yes")) {
+    $unsecure = $true
+    Set-Attr $result.win_join "unsecure" $unsecure.toString()
+}
+
+# Concat correct flags and options to Add-Computer command
+
+
+
 
 
 
@@ -65,7 +119,6 @@ If (($params.restart -eq "true") -or ($params.restart -eq "yes")) {
     Restart-Computer -Force
 }
 
-Set-Attr $result.win_join "host" $host.toString()
 Set-Attr $result.win_join "restart" $restart.toString()
 
 Exit-Json $result;

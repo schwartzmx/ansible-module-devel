7af733ce375792e4e872a2ca118baa27885f9809
schwartzmx <schwartzmx@gmail.com>
2015-01-15 09:50:08 -0600

    todo: remove disabled computer account in AD

--- windows/win_host.ps1
+++ windows/win_host.ps1
@@ -192,15 +192,8 @@ ElseIf ($hostname -and $domain){
                     # Remove from Domain
                     $cmd = "Remove-Computer $computername $workgroup $unjoincredential (New-Object System.Management.Automation.PSCredential $($user),(convertto-securestring $($pass) -asplaintext -force)) -Force"
                     Invoke-Expression $cmd
-                    Try {
-                        # Remove from AD
-                        $cmd = "Remove-ADComputer -Identity $computername $credential (New-Object System.Management.Automation.PSCredential $($user),(convertto-securestring $($pass) -asplaintext -force)) $server -Confirm:$false"
-                        Invoke-Expression $cmd
-                        $result.changed = $true
-                    }
-                    Catch {
-                        Fail-Json $result "an error occured when removing $hostname from AD computers. command attempted --> $cmd"
-                    }
+
+                    #TODO Remove Disabled Account from Computers Group in AD
                 }
                 Catch {
                     Fail-Json $result "an error occured when unjoining $hostname from domain. command attempted --> $cmd"
